a9b7518a2527a3f6130e9b475c207be0b5f747df
Sahil Ahmed <sa.idesofmarch@gmail.com>
2024-09-01 04:38:10 +0530

    lamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Navigation from "./components/navigation";
 import  Experiences  from "./components/experiences";
 import Name from "./components/name";
 import Intro from "./components/intro";
+import Spotlight from "./components/spotlight";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Intro/>
-      {/* <Name/> */}
+      <Spotlight/>
+      <Name/>
       {/* <Navigation /> */}
       {/* <Experiences/> */}
     </main>
