e1ba351a01a51f1790fefd244f005eced6c8abee
Sahil Ahmed <sa.idesofmarch@gmail.com>
2024-09-01 03:32:59 +0530

    init experiences component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Navigation from "./components/navigation";
+import  Experiences  from "./components/experiences";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      
-
-    
       <Navigation />
+      <Experiences/>
     </main>
   );
 }
