479d372805a7bf852af21dffa15112f9d5d9ae94
Sahil Ahmed <sa.idesofmarch@gmail.com>
2024-09-01 14:36:55 +0530

    add name and designation to socials and change padding/gap btw each

--- src/app/components/ui/animated-tooltip.tsx
+++ src/app/components/ui/animated-tooltip.tsx
@@ -41,7 +41,7 @@ export const AnimatedTooltip = ({
     <>
       {items.map((item, idx) => (
         <div
-          className="-mr-4  relative group"
+          className="px-1 relative group"
           key={item.name}
           onMouseEnter={() => setHoveredIndex(item.id)}
           onMouseLeave={() => setHoveredIndex(null)}
