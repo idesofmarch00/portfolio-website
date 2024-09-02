07dadb53dc95647f7be061140387966451bb8f5a
Sahil Ahmed <sa.idesofmarch@gmail.com>
2024-09-03 00:14:42 +0530

    headerfix

--- src/app/components/ui/floating-navbar.tsx
+++ src/app/components/ui/floating-navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   motion,
   AnimatePresence,
@@ -8,6 +8,7 @@ import {
 } from "framer-motion";
 import { cn } from "../../lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const FloatingNav = ({
   navItems,
@@ -21,11 +22,10 @@ export const FloatingNav = ({
   className?: string;
 }) => {
   const { scrollYProgress } = useScroll();
-
   const [visible, setVisible] = useState(false);
+  const pathname = usePathname();
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
     if (typeof current === "number") {
       let direction = current! - scrollYProgress.getPrevious()!;
 
@@ -40,6 +40,19 @@ export const FloatingNav = ({
       }
     }
   });
+
+  const getGradientColor = (path: string) => {
+    switch (path) {
+      case "/":
+        return "from-transparent via-blue-500 to-transparent";
+      case "/resume":
+        return "from-transparent via-green-500 to-transparent";
+      case "/contact":
+        return "from-transparent via-purple-500 to-transparent";
+      default:
+        return "from-transparent via-blue-500 to-transparent";
+    }
+  };
 
   return (
     <AnimatePresence mode="wait">
@@ -56,7 +69,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
+          "flex w-80 fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] py-2 items-center justify-center space-x-4",
           className
         )}
       >
@@ -65,17 +78,19 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500 group",
+              pathname === navItem.link ? "text-white bg-gradient-to-r from-blue-500 to-purple-500 text-white border-transparent border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full" : ""
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
             <span className="hidden sm:block text-sm">{navItem.name}</span>
+            <span className={cn(
+              "absolute inset-x-0 w-full mx-auto -bottom-px bg-gradient-to-r h-px opacity-0 group-hover:opacity-100 transition-opacity",
+              getGradientColor(navItem.link),
+              pathname === navItem.link ? "opacity-100 " : ""
+            )} />
           </Link>
         ))}
-        <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
-          <span>Login</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button>
       </motion.div>
     </AnimatePresence>
   );
